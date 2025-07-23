39f38fe0911f6d3414d2336bcb37a953119b12a9
Aditya Domle <adityadomle14@gmail.com>
2025-07-24 01:05:09 +0530

    develop plain components and apply grid layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
+import Offers from './Components/Offers';
+import Plan from './Components/Plan';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Offers />
+      <Plan />
     </div>
   );
 }
