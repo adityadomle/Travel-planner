4afe276595a8ea383a7ac6f3c6ebc0a8c067aa0a
Aditya Domle <adityadomle14@gmail.com>
2025-07-23 23:58:19 +0530

    Enhance image with Tailwind hover and transition effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import Hero from './Components/Hero';
 
 function App() {
   return (
-    <div>
-      <img src="https://images.unsplash.com/photo-1734362826950-0b622d51d570?q=80&w=1167&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    <div className='w-full h-screen'>
+      <img className='top-0 left-0 w-full h-screen object-cover' src="https://i.pinimg.com/1200x/0e/9a/8a/0e9a8a987492dbc3bfe06d25c861f5fc.jpg" alt="" />
+      <div className='bg-black/30 absolute top-0 left-0 w-full h-screen' />
+      <div className='absolute top-0 w-full flex flex-col justify-center text-white'>
+        <div className='md:left-[10%] max-w-[1100px] m-auto p-4'>
+          <p>All Inclusive</p>
+          <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>Private Beaches & Gateways</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero sed odit quasi veniam, tenetur cum illo ab delectus corrupti eaque maxime perferendis!</p>
+          <button>Reserve Now</button>
+        </div>
+      </div>
     </div>
   )
 }
