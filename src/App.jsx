013687bb17d4041024779e4f34ae65becc121e69
Aditya Domle <adityadomle14@gmail.com>
2025-07-23 23:15:13 +0530

    initialize React app with Tailwind CSS and basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './Components/Hero';
+
+function App() {
+  return (
+    <div>
+      <img src="https://images.unsplash.com/photo-1734362826950-0b622d51d570?q=80&w=1167&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    </div>
+  )
+}
+
+export default App;
